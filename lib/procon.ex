cf14feabc4f8308bf04363a81ec959ddd03e904a
s_arme <s.arme@ya.ru>
2021-02-13 03:27:06 +0330

    implemement init method and call async produce endpoint

--- lib/procon.ex
+++ lib/procon.ex
@@ -4,4 +4,9 @@ defmodule Procon.Producer do
   def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: {:global, :producer})
   end
+  # server side /callback functions
+  def init(i) do
+    GenServer.cast({:global, :producer}, :produce)
+    {:ok, i}
+  end
 end
