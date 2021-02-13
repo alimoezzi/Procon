d143ec0475bc2da3193788ceec5126f4edcd6021
s_arme <s.arme@ya.ru>
2021-02-13 03:30:27 +0330

    Impelement :get handle call in case of enough remaining item

--- lib/procon.ex
+++ lib/procon.ex
@@ -14,6 +14,12 @@ defmodule Procon.Producer do
     GenServer.cast({:global, :producer}, :produce)
     {:ok, i}
   end
+
+  def handle_call(:get, _from, state) when length(state) > 0 do
+    # {:atom, oldstate, newstate}
+    [h | t] = state
+    {:reply, h, t}
+  end
   def handle_cast(:produce, state) do
     spawn(fn -> Process.sleep(@delay) && GenServer.cast({:global, :producer}, :produce) end)
     {:noreply, state ++ Enum.to_list(50..(2 * 50 + 1))}
